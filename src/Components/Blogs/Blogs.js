10d4556062a2ff345df457af027223e5bf984bfc
FazlulHaqueRazaChowdhury <fozlulcoc1592@gmail.com>
2022-04-18 03:40:12 +0600

    answered 2 questions

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -9,7 +9,9 @@ const Blogs = () => {
                 <Header></Header>
                 <div className="container">
                     <h1 className='special text-white'> Why are you using firebase? What other options do you have to implement authentication?</h1>
+                    <p className='text-secondary'>I'm Using Firebase for user authentication.So I can implement sign in and sign up. Except firebase there are also  some like Auth0 ,Prove, Okta, KeyCloack,etc. We can also those alternative of Firebase. </p>
                     <h1 className='special text-white'>  What other services does firebase provide other than authentication?</h1>
+                    <p>Except Authentication we can also use Cloud Firestore to sotre and sync app data. They also provides Cloud Functions. That run our code in the cloud with no servers or containers to manage. We can also host our websites in Firebase. We store data on Cloud Storage. There are also Cloud Messaging, Google Analytics and more.  </p>
                 </div>
             </div>
 
